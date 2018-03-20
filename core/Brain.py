4d23d1372f97cc4c4caf8211b605398b138ceaeb
lynnxi <lynn.xi@yahoo.com>
2018-03-21 03:03:34 +0800

    add game flow

--- core/Brain.py
+++ core/Brain.py
@@ -7,6 +7,42 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\t
 from matplotlib import pyplot as plt
 from PIL import Image, ImageFilter, ImageGrab
 
+
+PREFLOP = 1
+FLOP = 2
+TURN = 3
+RIVER = 4
+S6 = 1
+S12 = 2
+CHECK = 1
+BET = 2
+CALL = 3
+FOLD = 4
+
+round_desc = {
+    PREFLOP : 'Preflop',
+    FLOP : 'Flop',
+    TURN : 'Turn',
+    RIVER : 'River',
+}
+player_desc = {
+    S6: 's6',
+    S12 : 's12',
+}
+action_desc = {
+    CHECK : 'Check',
+    BET : 'Bet',
+    CALL : 'Call',
+    FOLD : 'Fold',
+}
+
+round_card_no = {
+    PREFLOP: 0,
+    FLOP: 3,
+    TURN: 4,
+    RIVER: 5,
+}
+
 class Brain(object):
     def __init__(self):
         self.poker_identify = '../pics/pokermaster_identify.png'
@@ -16,16 +52,24 @@ class Brain(object):
         self.fold_pic = '../pics/FOLD.png'
         self.check_pic = '../pics/CHECK.png'
         self.is_find_table = False
+        self.players_seq = [S6, S12]
+        self.action_on = S12
+        self.btn = False
+        self.bb = False
+        self.hands_num = 0
+        self.round_last_action = False
+        self.game_stage = PREFLOP
+        self.hands_history = [{PREFLOP: [], FLOP: [], TURN: [], RIVER: []}]
         self.position_config = {
-            'table_card' : [127, 470, 437, 560],
+            'table_card' : [127, 470, 470, 560],
             'players' : {
-                's12': {
+                S12: {
                     'a' : [190, 15, 390, 200],
                     'name' : [60, 15, 160, 47],
                     'money' : [70, 120, 145, 145],
                     'bet' : [20, 145, 95, 170],
                 },
-                's6':{
+                S6:{
                     'a': [135, 595, 440, 880],
                     'name': [115, 125, 215, 147],
                     'money': [125, 220, 205, 245],
@@ -48,10 +92,10 @@ class Brain(object):
                     print("Card template File not found: " + str(x) + str(y) + ".png")
 
         self.action_images = {
-            'bet': cv2.cvtColor(np.array(Image.open('../pics/BET.png')), cv2.COLOR_BGR2RGB),
-            'call': cv2.cvtColor(np.array(Image.open('../pics/CALL.png')), cv2.COLOR_BGR2RGB),
-            'check': cv2.cvtColor(np.array(Image.open('../pics/CHECK.png')), cv2.COLOR_BGR2RGB),
-            'fold': cv2.cvtColor(np.array(Image.open('../pics/FOLD.png')), cv2.COLOR_BGR2RGB),
+            BET: cv2.cvtColor(np.array(Image.open('../pics/BET.png')), cv2.COLOR_BGR2RGB),
+            CALL: cv2.cvtColor(np.array(Image.open('../pics/CALL.png')), cv2.COLOR_BGR2RGB),
+            CHECK: cv2.cvtColor(np.array(Image.open('../pics/CHECK.png')), cv2.COLOR_BGR2RGB),
+            FOLD: cv2.cvtColor(np.array(Image.open('../pics/FOLD.png')), cv2.COLOR_BGR2RGB),
         }
 
     def match_template(self, screenshot, template):
@@ -77,7 +121,7 @@ class Brain(object):
         count = 0
         points = []
         for pt in zip(*loc[::-1]):
-            cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (7,249,151), 2)
+            #cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (7,249,151), 2)
             count += 1
             points.append(pt)
 
@@ -89,9 +133,9 @@ class Brain(object):
         return count, points, best_fit, min_val
 
     def find_table(self, screenshot):
-        cont, points, best_fit, min_val = self.match_template(screenshot, Image.open(self.poker_identify))
+        count, points, best_fit, min_val = self.match_template(screenshot, Image.open(self.poker_identify))
 
-        return cont >= 1
+        return count >= 1
 
     def check_table_card(self, screenshot):
         self.cards_on_table = []
@@ -99,33 +143,74 @@ class Brain(object):
         table_card_img = screenshot.crop((table_card_position[0], table_card_position[1],
                                     table_card_position[2], table_card_position[3]))
 
-        #cv2.imshow('Detected', np.array(table_card_img))
-        #cv2.waitKey(0)
-        #cv2.destroyAllWindows()
+        # cv2.imshow('Detected', np.array(table_card_img))
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
         for key, template in self.card_images.items():
             count, points, best_fit, min_val = self.match_template(table_card_img, template)
             if count >= 1:
                 self.cards_on_table.append(key)
 
-        self.game_stage = ''
+        # self.game_stage = PREFLOP
+        #
+        # if len(self.cards_on_table) < 1:
+        #     self.game_stage = PREFLOP
+        # elif len(self.cards_on_table) == 3:
+        #     self.game_stage = FLOP
+        # elif len(self.cards_on_table) == 4:
+        #     self.game_stage = TURN
+        # elif len(self.cards_on_table) == 5:
+        #     self.game_stage = RIVER
 
-        if len(self.cards_on_table) < 1:
-            self.game_stage = "PreFlop"
-        elif len(self.cards_on_table) == 3:
-            self.game_stage = "Flop"
-        elif len(self.cards_on_table) == 4:
-            self.game_stage = "Turn"
-        elif len(self.cards_on_table) == 5:
-            self.game_stage = "River"
+    def check_player_on_action(self, screenshot, player):
+        position_a = self.position_config['players'][player]['a']
+        position_money = self.position_config['players'][player]['money']
+
+        imga = screenshot.crop((position_a[0], position_a[1],
+                                position_a[2], position_a[3]))
+
+        imgm = screenshot.crop((position_money[0], position_money[1],
+                             position_money[2], position_money[3]))
+        # cv2.imshow('Detected', np.array(s12_img))
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+        #
+        # cv2.imshow('Detected', np.array(s6_img))
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+        #
+        # cv2.imshow('Detected', np.array(s12_money))
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+        #
+        # cv2.imshow('Detected', np.array(s6_money))
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+        #self.players[player]['name'] = player;
+        self.players[player]['money'] = pytesseract.image_to_string(imgm);
+
+        action = False
+        bet_size = False
+        for key, template in self.action_images.items():
+            count, points, best_fit, min_val = self.match_template(imga, template)
+            if count >= 1:
+                self.players[player]['action'] = key
+                action = key
+                if key == BET:
+                    bet_size = self.check_betsize(imga, player)
+                break
+
+        return action, bet_size
 
     def check_player_status(self, screenshot):
-        self.players = {'s12' : {}, 's6' : {}}
-        player_position_s6 = self.position_config['players']['s6']['a']
-        player_position_s12 = self.position_config['players']['s12']['a']
-        player_position_s6_name = self.position_config['players']['s6']['name']
-        player_position_s12_name = self.position_config['players']['s12']['name']
-        player_position_s6_money = self.position_config['players']['s6']['money']
-        player_position_s12_money = self.position_config['players']['s12']['money']
+        self.players = {S12 : {}, S6 : {}}
+        player_position_s6 = self.position_config['players'][S6]['a']
+        player_position_s12 = self.position_config['players'][S12]['a']
+        player_position_s6_name = self.position_config['players'][S6]['name']
+        player_position_s12_name = self.position_config['players'][S12]['name']
+        player_position_s6_money = self.position_config['players'][S6]['money']
+        player_position_s12_money = self.position_config['players'][S12]['money']
 
         s12_img = screenshot.crop((player_position_s12[0], player_position_s12[1],
                                    player_position_s12[2], player_position_s12[3]))
@@ -159,31 +244,44 @@ class Brain(object):
         # cv2.waitKey(0)
         # cv2.destroyAllWindows()
 
-        self.players['s12']['name'] = 's12';
-        self.players['s6']['name'] = 's6';
-        self.players['s12']['money'] = pytesseract.image_to_string(s12_money);
-        self.players['s6']['money'] = pytesseract.image_to_string(s6_money);
+        self.players[S12]['name'] = 's12';
+        self.players[S6]['name'] = 's6';
+        self.players[S12]['money'] = pytesseract.image_to_string(s12_money);
+        self.players[S6]['money'] = pytesseract.image_to_string(s6_money);
 
-        cont, points, best_fit, min_val = self.match_template(s6_img, Image.open(self.btn_pic))
-        if cont >= 1:
-            self.players['s6']['btn'] = True
-            self.players['s12']['btn'] = False
+        count, points, best_fit, min_val = self.match_template(s6_img, Image.open(self.btn_pic))
+        if count >= 1:
+            if self.btn != S6 and len(self.cards_on_table) == round_card_no[PREFLOP]:
+                self.new_hand(S6, S12)
+        else:
+            if self.btn != S12 and len(self.cards_on_table) == round_card_no[PREFLOP]:
+                self.new_hand(S12, S6)
+
 
         for key, template in self.action_images.items():
             count, points, best_fit, min_val = self.match_template(s6_img, template)
             if count >= 1:
-                self.players['s6']['action'] = key
-                if key == 'bet':
-                    self.check_betsize(s6_img, 's6')
+                self.players[S6]['action'] = key
+                if key == BET:
+                    self.check_betsize(s6_img, S6)
                 break
 
         for key, template in self.action_images.items():
             count, points, best_fit, min_val = self.match_template(s12_img, template)
             if count >= 1:
-                self.players['s12']['action'] = key
-                if key == 'bet':
-                    self.check_betsize(s12_img, 's12')
+                self.players[S12]['action'] = key
+                if key == BET:
+                    self.check_betsize(s12_img, S12)
                 break
+
+    def new_hand(self, btn, bb):
+        self.hands_num += 1
+        self.players_seq = [bb, btn]
+        self.hands_history.insert(self.hands_num, {PREFLOP: [], FLOP: [], TURN: [], RIVER: []})
+        self.btn = btn
+        self.bb = bb
+        self.game_stage = PREFLOP
+        self.round_last_action = False
 
     def check_betsize(self, screenshot, player):
         bet_size_position = self.position_config['players'][player]['bet']
@@ -194,8 +292,13 @@ class Brain(object):
         # cv2.imshow('Detected', np.array(img))
         # cv2.waitKey(0)
         # cv2.destroyAllWindows()
+        #while true:
+        bet_size = pytesseract.image_to_string(img);
+        #    if (bet_size.isdigit()):
+        #        break;
 
-        self.players[player]['betsize'] = pytesseract.image_to_string(img);
+        self.players[player]['betsize'] = bet_size;
+        return bet_size
 
     def check_pot(self, screenshot):
         pot_postion = self.position_config['pot']
@@ -210,6 +313,7 @@ class Brain(object):
 
 
     def start(self, q):
+         i = 0
          while True:
              s = q.get(True)
              # print('get a sth from eyes')
@@ -219,8 +323,80 @@ class Brain(object):
                     self.is_find_table = True
              else:
                  self.check_table_card(s)
-                 print ('table stage', self.game_stage, 'cards on table', self.cards_on_table)
                  self.check_player_status(s)
-                 print ('players', self.players)
-                 self.check_pot(s)
-                 print ('pot', self.pot)
+                 if self.btn == False:
+                     continue
+                 # if self.game_stage == PREFLOP:
+                 #     if len(self.hands_history[self.hands_num][self.game_stage]) == 0:
+                 #        self.action_on = self.btn
+                 # else:
+                 #     if len(self.hands_history[self.hands_num][self.game_stage]) == 0:
+                 #        self.action_on = self.bb
+                 #
+                 # action, bet_size = self.check_player_on_action(s, self.action_on)
+                 # if action:
+                 #     action_data = {'palyer': player_desc[self.action_on], 'action': action_desc[action], 'bet_size': bet_size}
+                 #     self.hands_history[self.hands_num][self.game_stage].append(action_data)
+                 #     self.action_on = self.players_seq[(self.players_seq.index(self.action_on) + 1) % 2]
+                 #     self.check_pot(s)
+                 #     print (
+                 #     'table stage', round_desc[self.game_stage], 'cards on table', self.cards_on_table, 'action', action_data, 'pot', self.pot)
+                 if self.game_stage == PREFLOP:
+                     if len(self.hands_history[self.hands_num][self.game_stage]) == 0:
+                        self.action_on = self.btn
+                     action, bet_size = self.check_player_on_action(s, self.action_on)
+                     if action == False :
+                         continue
+
+                     action_data = {'palyer': player_desc[self.action_on], 'action': action_desc[action],
+                                    'bet_size': bet_size}
+                     self.hands_history[self.hands_num][self.game_stage].append(action_data)
+                     self.action_on = self.players_seq[(self.players_seq.index(self.action_on) + 1) % 2]
+                     self.check_pot(s)
+                     print (
+                         'table stage', round_desc[self.game_stage], 'cards on table', self.cards_on_table,
+                         'action', action_data, 'pot', self.pot)
+
+                     if action == BET:
+                         self.round_last_action = BET
+                     elif action == CALL and self.round_last_action == BET:
+                         self.game_stage = FLOP
+                         self.round_last_action = False
+                     elif action == CHECK:
+                         self.game_stage = FLOP
+                         self.round_last_action = False
+                 else:
+                     if len(self.cards_on_table) < round_card_no[self.game_stage]:
+                         continue
+                     if len(self.hands_history[self.hands_num][PREFLOP]) == 0:
+                         continue
+                     if len(self.hands_history[self.hands_num][self.game_stage]) == 0:
+                        self.action_on = self.bb
+                     action, bet_size = self.check_player_on_action(s, self.action_on)
+
+                     if action == False :
+                         continue
+
+                     action_data = {'palyer': player_desc[self.action_on], 'action': action_desc[action],
+                                    'bet_size': bet_size}
+                     self.hands_history[self.hands_num][self.game_stage].append(action_data)
+                     self.action_on = self.players_seq[(self.players_seq.index(self.action_on) + 1) % 2]
+                     self.check_pot(s)
+                     print (
+                         'table stage', round_desc[self.game_stage], 'cards on table', self.cards_on_table,
+                         'action', action_data, 'pot', self.pot)
+
+                     if action == BET:
+                         self.round_last_action = BET
+                     elif action == CALL:
+                         self.game_stage = TURN if self.game_stage == FLOP else RIVER
+                         self.round_last_action = False
+                     elif action == CHECK:
+                         if self.round_last_action == CHECK:
+                             self.game_stage = TURN if self.game_stage == FLOP else RIVER
+                             self.round_last_action = False
+                         else:
+                             self.round_last_action = CHECK
+
+
+
--- test/test.py
+++ test/test.py
@@ -1,12 +1,36 @@
-import wx
-import win32gui
-class Desktop(wx.Frame):
-    def __init__(self):
-        super(Desktop,self).__init__(None,-1,'')
-        self.AssociateHandle(win32gui.GetDesktopWindow())
-        dc = wx.WindowDC(self)
-        dc.DrawRectangle( 100, 100, 800, 800 )
+def CFR(action_history, reach_probs, player_cards):
 
-app = wx.App(0)
-mf = Desktop()
-app.MainLoop()
+    if isTerminal(action_history):  # 是不是到了叶子节点
+        current_player = len(action_history) % 2
+        return getUtility(action_history, player_cards, current_player)
+
+    counterfactual_value_h = 0.0
+    counterfactual_value_h_a = [0.0, 0.0]
+    current_player = len(action_history) % 2  # 根据当前action history 计算infomation set
+    info_set = str(player_cards[current_player])
+    for i in range(len(action_history)):
+        info_set += str(action_history[i])
+
+    for i in range(2):  # 两种可能的行动 pass 0; bet 1;
+        new_action_history = action_history
+        new_action_history.append(i)
+        new_reach_probs = reach_probs
+        new_reach_probs[current_player] = new_reach_probs[current_player] * cur_strategy[info_set + str(i)]
+        counterfactual_value_h_a[i] = CFR(new_action_history, new_reach_probs, player_cards)
+        counterfactual_value_h += cur_strategy[info_set + str(i)] * counterfactual_value_h_a[i]
+
+    regretSum = 0.0
+    for i in range(2):
+        regret[info_set + str(i)] += reach_probs[1 - current_player] * (
+                counterfactual_value_h_a[i] - counterfactual_value_h)
+        AccStrategy[info_set + str(i)] += reach_probs[current_player] * cur_strategy[info_set + str(i)]
+        if regret[info_set + str(i)] > 0:
+            regretSum += regret[info_set + str(i)]
+    for i in range(2):
+        if regretSum > 0:
+            cur_strategy[info_set + str(i)] = max(0, regret[info_set + str(i)]) / regretSum
+        else:
+            cur_strategy[info_set + str(i)] = 0.5
+        strategySum[info_set + str(i)] += cur_strategy[info_set + str(i)]
+
+    return counterfactual_value_h
