0f5d9feaa0ffa074b32f80ef7760ad12200f5069
lynnxi <lynn.xi@yahoo.com>
2018-03-24 10:48:08 +0800

    edit spell

--- core/Holdem.py
+++ core/Holdem.py
@@ -12,7 +12,7 @@ class Holdem(object):
                 self.cards.append(x + y)
         self.bet_size = [0.3, 0.5, 0.7, 1, 1.5, 10]
         self.roles = ['oop', 'ip']
-        self.deccision_root = {
+        self.decision_root = {
             'bet' : {},
             'call' : {},
             'fold' : {},
@@ -27,48 +27,48 @@ class Holdem(object):
             'is_leaf' : False,
         }
 
-    def make_decision_tree(self, deccision_node):
-        if deccision_node['is_leaf'] :
+    def make_decision_tree(self, decision_node):
+        if decision_node['is_leaf'] :
             return
-        if deccision_node['need_to_call'] < deccision_node['my_chip_stacks']:
-            new_role = deccision_node['role'] % 2
+        if decision_node['need_to_call'] < decision_node['my_chip_stacks']:
+            new_role = decision_node['role'] % 2
             node_bet = self.make_new_action_node(new_role,
-                                                 deccision_node['stacks' + OOP],
-                                                 deccision_node['stacks' + IP],
-                                                 deccision_node['round'],
-                                                 bet_size - deccision_node['need_to_call'],
-                                                 deccision_node['pot'] + bet_size)
-            node_bet['stacks' + deccision_node['role']] = node_bet['stacks' + deccision_node['role']] - bet_size
-            deccision_node['bet'] = self.make_decision_tree(node_bet)
+                                                 decision_node['stacks' + OOP],
+                                                 decision_node['stacks' + IP],
+                                                 decision_node['round'],
+                                                 bet_size - decision_node['need_to_call'],
+                                                 decision_node['pot'] + bet_size)
+            node_bet['stacks' + decision_node['role']] = node_bet['stacks' + decision_node['role']] - bet_size
+            decision_node['bet'] = self.make_decision_tree(node_bet)
 
-        if deccision_node['need_to_call'] > 0:
-            node = self.make_next_round_node(deccision_node['round'],
-                                             deccision_node['stacks' + OOP],
-                                             deccision_node['stacks' + IP],
-                                             deccision_node['pot'] + deccision_node['need_to_call'])
-            node['stacks' + deccision_node['role']] = node['stacks' + deccision_node['role']] - deccision_node['need_to_call']
-            deccision_node['call'] = self.make_decision_tree(node)
+        if decision_node['need_to_call'] > 0:
+            node = self.make_next_round_node(decision_node['round'],
+                                             decision_node['stacks' + OOP],
+                                             decision_node['stacks' + IP],
+                                             decision_node['pot'] + decision_node['need_to_call'])
+            node['stacks' + decision_node['role']] = node['stacks' + decision_node['role']] - decision_node['need_to_call']
+            decision_node['call'] = self.make_decision_tree(node)
 
-        if deccision_node['need_to_call'] == 0:
-            if deccision_node['role'] == IP:
-                node = self.make_next_round_node(deccision_node['round'],
-                                                 deccision_node['stacks' + OOP],
-                                                 deccision_node['stacks' + IP],
-                                                 deccision_node['pot'] + deccision_node['need_to_call'])
-                node['stacks' + deccision_node['role']] = node['stacks' + deccision_node['role']] - deccision_node['need_to_call']
-                deccision_node['call'] = self.make_decision_tree(node)
+        if decision_node['need_to_call'] == 0:
+            if decision_node['role'] == IP:
+                node = self.make_next_round_node(decision_node['round'],
+                                                 decision_node['stacks' + OOP],
+                                                 decision_node['stacks' + IP],
+                                                 decision_node['pot'] + decision_node['need_to_call'])
+                node['stacks' + decision_node['role']] = node['stacks' + decision_node['role']] - decision_node['need_to_call']
+                decision_node['call'] = self.make_decision_tree(node)
 
             else:
-                node = self.make_new_action_node(deccision_node['role'] % 2,
-                                                 deccision_node['stacks' + OOP],
-                                                 deccision_node['stacks' + IP],
-                                                 deccision_node['round'],
+                node = self.make_new_action_node(decision_node['role'] % 2,
+                                                 decision_node['stacks' + OOP],
+                                                 decision_node['stacks' + IP],
+                                                 decision_node['round'],
                                                  0,
-                                                 deccision_node['pot'])
-                deccision_node['call'] = self.make_decision_tree(node)
+                                                 decision_node['pot'])
+                decision_node['call'] = self.make_decision_tree(node)
 
 
-        deccision_tree['fold'] = self.make_leaf_node()
+        decision_tree['fold'] = self.make_leaf_node()
 
 
     def make_leaf_node(self):
